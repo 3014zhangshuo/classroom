425494fcfc69e4b863b2910344de301ccf207233
张硕 <zhangshuo@zhangshuodeMBP.lan>
2016-12-14 15:19:47 +0800

    完成destory的test

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,34 +1,48 @@
 class CoursesController < ApplicationController
-  def index
-    @courses = Course.all
+    def index
+        @courses = Course.all
+    end
+
+    def show
+        @course = Course.find(params[:id])
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+        @course = Course.new(course_params)
+
+        if @course.save
+            redirect_to courses_path
+        else
+            render :new
+        end
   end
 
-  def show
+    def edit
+        @course = Course.find(params[:id])
+    end
+
+    def update
     @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      redirect_to course_path(@course)
+    else
+      render :edit
+    end
   end
 
-  def new
-    @course = Course.new
-  end
-
-  def create
-  @course = Course.new(course_params)
-
-  if @course.save
+  def destroy
+    @course = Course.find(params[:id])
+    @course.delete
     redirect_to courses_path
-  else
-    render :new
   end
-end
+    private
 
-  def edit
-    @course = Course.find(params[:id])
-  end
-
-private
-
-  def course_params
-    params.require(:course).permit(:title, :description)
-  end
-
+    def course_params
+        params.require(:course).permit(:title, :description)
+    end
 end
